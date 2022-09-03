e85623ff54a4893667c4172781d2d27051330e7e
flavie simon <flavie.simon@hotmail.fr>
2022-09-03 20:33:05 +0200

    udpate if name=main in other_data.py for testing

--- brainmnist/other_data.py
+++ brainmnist/other_data.py
@@ -25,6 +25,7 @@ def map_other_data(data: pd.DataFrame) -> pd.DataFrame:
     data.columns = ['index_event', 'channel', 'true_digit', 'eeg'] #rename columns
     data = data.reindex(columns=['index_event', 'true_digit', 'channel', 'eeg']) #reorder columns
 
+    df = df.sort_values(by='index_event').iloc[:40,:]
     #dispatch eeg signals in multiple columns
     for i in range(max_data_points):
         data = pd.concat([data, pd.DataFrame(data.iloc[:,3]).apply(lambda x: int(x.str.split(',').iloc[0][i]), axis=1)], axis=1)
@@ -37,7 +38,6 @@ def map_other_data(data: pd.DataFrame) -> pd.DataFrame:
 
 if __name__=='__main__':
     df = load_other_data()
-    df = df.sort_values(by='index_event').iloc[:20000,:]
     df = map_other_data(df)
     # df = balance_data(df)
     # X, y = map_data_array3D(df)
