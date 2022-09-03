9b64ffa88eb6a48b2f1378e62317c1e7f8120d2b
flavie simon <flavie.simon@hotmail.fr>
2022-09-03 13:10:19 +0200

    add function load_clean_data_from_bucket to data.py

--- brainmnist/data.py
+++ brainmnist/data.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
+
 def load_data() -> pd.DataFrame:
     """
     load data from txt format
     """
     data = pd.read_csv('drive/MyDrive/Brain/data/MU2.txt', sep=',', header=None)
     return data
+
 
 
 def map_data(data: pd.DataFrame) -> pd.DataFrame:
@@ -67,20 +69,50 @@ def map_data(data: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+
+def load_clean_data_from_bucket() -> pd.DataFrame:
+    """
+    Load clean data (ie mapped) from bucket
+    """
+
+    BUCKET_NAME = "brain-mnist"
+    data_file_in_bucket = 'MU2_clean.csv'
+    df = pd.read_csv(f"gs://{BUCKET_NAME}/{data_file_in_bucket}")
+
+    return df
+
+
+
 def balance_data(df: pd.DataFrame) -> pd.DataFrame:
     """
-    balance data acoording to the less represented digit
+    Balance data acoording to the less represented digit
     """
 
     #number of representations of the digit the less represented for each electrode (TP9 is chosen arbitrarily, no impact)
     min_count_digits = df[df.channel=='TP9'].groupby('true_digit').count().iloc[:,0].min()
 
     df_concat = pd.DataFrame()
+
     #sample min_count_digits different index_event for true_digit from -1 to 9
     for i in range(-1,10):
-    events_of_digit = df[df.channel=='TP9'][df[df.channel=='TP9'].true_digit==i]['index_event'] #identify all events related to the true_digit considered
-    sample_events_of_digit = events_of_digit.sample(min_count_digits,replace=False).tolist() #sample min_count_digits events among all events related to true_digit considered
-    df_sample_digit = df[df.index_event.isin(sample_events_of_digit)] #keep only events that were sampled for the digit
-    df_concat = pd.concat([df_concat, df_sample_digit]) #concatenate events that were sampled for all  digits
+
+        #identify all events related to the true_digit considered
+        events_of_digit = df[df.channel=='TP9'][df[df.channel=='TP9'].true_digit==i]['index_event']
+
+        #sample min_count_digits events among all events related to true_digit considered
+        sample_events_of_digit = events_of_digit.sample(min_count_digits,replace=False).tolist()
+
+        #keep only events that were sampled for the digit
+        df_sample_digit = df[df.index_event.isin(sample_events_of_digit)]
+
+        #concatenate events that were sampled for all  digits
+        df_concat = pd.concat([df_concat, df_sample_digit])
 
     return df_concat
+
+
+
+if __name__=='__main__':
+    df = load_clean_data_from_bucket()
+    df = balance_data(df)
+    print(df.shape)
