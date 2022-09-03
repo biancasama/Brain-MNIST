2e41fb9d5252e307aaaeeb2c1b232d48fad356b5
flavie simon <flavie.simon@hotmail.fr>
2022-09-03 20:29:36 +0200

    udpate if name=main in other_data.py for testing

--- brainmnist/other_data.py
+++ brainmnist/other_data.py
@@ -1,5 +1,3 @@
-
-
 import pandas as pd
 import numpy as np
 
@@ -22,23 +20,25 @@ def map_other_data(data: pd.DataFrame) -> pd.DataFrame:
     # min_data_points = data.iloc[:,5].min()
     max_data_points = data.iloc[:,5].max()
 
-    data = data.drop(columns=[0,2,5])
+    data = data.drop(columns=[0,2,5]) #drop useless columns
 
-    data.columns = ['index_event', 'channel', 'true_digit', 'eeg']
-    data = data.reindex(columns=['index_event', 'true_digit', 'channel', 'eeg'])
+    data.columns = ['index_event', 'channel', 'true_digit', 'eeg'] #rename columns
+    data = data.reindex(columns=['index_event', 'true_digit', 'channel', 'eeg']) #reorder columns
 
+    #dispatch eeg signals in multiple columns
     for i in range(max_data_points):
         data = pd.concat([data, pd.DataFrame(data.iloc[:,3]).apply(lambda x: int(x.str.split(',').iloc[0][i]), axis=1)], axis=1)
         data.columns = list(data.columns[:-1]) + [i]
-
     data = data.drop(columns='eeg')
 
     return data
 
 
+
 if __name__=='__main__':
     df = load_other_data()
-    df = map_other_data(df)
+    print(df.shape)
+    # df = map_other_data(df)
     # df = balance_data(df)
     # X, y = map_data_array3D(df)
     # print(X.shape)
