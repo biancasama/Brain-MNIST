6c488f1d1b15492886f4e1d9070edcc49f8c97f3
flavie simon <flavie.simon@hotmail.fr>
2022-09-05 16:52:34 +0200

    add load_images

--- brainmnist/load_images.py
+++ brainmnist/load_images.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+# import tensorflow as tf
 import pathlib
 import numpy as np
 import os
@@ -10,47 +10,48 @@ image_count = len(list(data_dir.glob('*/*.npy')))
 print(image_count)
 
 list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'), shuffle=False)
-list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
+print(list_ds)
+# list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
 
-class_names = np.array(sorted([item.name for item in data_dir.glob('*/*.npy') if item.name != ".ipynb_checkpoints"]))
-class_names = np.array([label.replace('.npy', '').split('_')[-1] for label in class_names])
+# class_names = np.array(sorted([item.name for item in data_dir.glob('*/*.npy') if item.name != ".ipynb_checkpoints"]))
+# class_names = np.array([label.replace('.npy', '').split('_')[-1] for label in class_names])
 
-val_size = int(image_count * 0.3)
-train_ds = list_ds.skip(val_size)
-val_ds = list_ds.take(val_size)
+# val_size = int(image_count * 0.3)
+# train_ds = list_ds.skip(val_size)
+# val_ds = list_ds.take(val_size)
 
-AUTOTUNE = tf.data.AUTOTUNE
-img_height = 192
-img_width = 256
+# AUTOTUNE = tf.data.AUTOTUNE
+# img_height = 192
+# img_width = 256
 
-def get_label(file_path):
-  # Convert the path to a list of path components
-  parts = tf.strings.split(file_path, os.path.sep)
-  # The second to last is the class-directory
-  file_name = parts[-1]
-  label = tf.strings.split(file_name, sep='_', maxsplit=-1, name=None)[-1]
+# def get_label(file_path):
+#   # Convert the path to a list of path components
+#   parts = tf.strings.split(file_path, os.path.sep)
+#   # The second to last is the class-directory
+#   file_name = parts[-1]
+#   label = tf.strings.split(file_name, sep='_', maxsplit=-1, name=None)[-1]
 
-  label = tf.strings.regex_replace(
-    label, '.npy', '', replace_global=True, name=None)
+#   label = tf.strings.regex_replace(
+#     label, '.npy', '', replace_global=True, name=None)
 
-  return label
+#   return label
 
-def decode_img(img):
-  image = tf.numpy_function(np.load, [img], tf.uint8)
-  return image
+# def decode_img(img):
+#   image = tf.numpy_function(np.load, [img], tf.uint8)
+#   return image
 
-def process_path(file_path):
-  label = get_label(file_path)
+# def process_path(file_path):
+#   label = get_label(file_path)
 
-  # Load the raw data from the file as a string
-  # img = tf.io.read_file(file_path)
-  img = decode_img(file_path)
+#   # Load the raw data from the file as a string
+#   # img = tf.io.read_file(file_path)
+#   img = decode_img(file_path)
 
-  return img, label
+#   return img, label
 
-train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+# train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+# val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 
-for image, label in train_ds.take(1):
-  print("Image shape: ", image.numpy().shape)
-  print("Label: ", label.numpy())
+# for image, label in train_ds.take(1):
+#   print("Image shape: ", image.numpy().shape)
+#   print("Label: ", label.numpy())
