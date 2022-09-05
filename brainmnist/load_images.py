8505020d22e80186e4511a28abccce95994cb39c
flavie simon <flavie.simon@hotmail.fr>
2022-09-05 17:35:37 +0200

    add load_images

--- brainmnist/load_images.py
+++ brainmnist/load_images.py
@@ -25,33 +25,33 @@ img_height = 192
 img_width = 256
 
 def get_label(file_path):
-  # Convert the path to a list of path components
-  parts = tf.strings.split(file_path, os.path.sep)
-  # The second to last is the class-directory
-  file_name = parts[-1]
-  label = tf.strings.split(file_name, sep='_', maxsplit=-1, name=None)[-1]
+    # Convert the path to a list of path components
+    parts = tf.strings.split(file_path, os.path.sep)
+    # The second to last is the class-directory
+    file_name = parts[-1]
+    label = tf.strings.split(file_name, sep='_', maxsplit=-1, name=None)[-1]
 
-  label = tf.strings.regex_replace(
+    label = tf.strings.regex_replace(
     label, '.npy', '', replace_global=True, name=None)
 
-  return label
+    return label
 
 def decode_img(img):
-  image = tf.numpy_function(np.load, [img], tf.uint8)
-  return image
+    image = tf.numpy_function(np.load, [img], tf.uint8)
+    return image
 
 def process_path(file_path):
-  label = get_label(file_path)
-/home/flavie/code/fla66/Brain-MNIST/data
-  # Load the raw data from the file as a string
-  # img = tf.io.read_file(file_path)
-  img = decode_img(file_path)
+    label = get_label(file_path)
 
-  return img, label
+    # Load the raw data from the file as a string
+    # img = tf.io.read_file(file_path)
+    img = decode_img(file_path)
+
+    return img, label
 
 train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 
 for image, label in train_ds.take(1):
-  print("Image shape: ", image.numpy().shape)
-  print("Label: ", label.numpy())
+    print("Image shape: ", image.numpy().shape)
+    print("Label: ", label.numpy())
