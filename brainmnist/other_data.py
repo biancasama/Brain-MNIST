dc4a9beb9e4ad8f1af90665a181f4aece95d3eb4
flavie simon <flavie.simon@hotmail.fr>
2022-09-07 07:26:48 +0200

    change map_other_data to be more efficient

--- brainmnist/other_data.py
+++ brainmnist/other_data.py
@@ -28,19 +28,41 @@ def map_other_data(data: pd.DataFrame) -> pd.DataFrame:
     data.columns = ['index_event', 'channel', 'true_digit', 'eeg'] #rename columns
     data = data.reindex(columns=['index_event', 'true_digit', 'channel', 'eeg']) #reorder columns
 
-    concat = pd.DataFrame()
-    for i in range(data.shape[0]):
-      if i%100==0: print('Row ' + str(i) + '/' + str(data.shape[0]))
-      concat = pd.concat([concat, pd.DataFrame(data.loc[i,'eeg'].split(',')).T], axis=0)
-    concat.index = list(range(0,data.shape[0]))
+    data = pd.concat([data.iloc[:,:3], pd.DataFrame(data.iloc[:,3].apply(lambda x: x.split(',')))], axis=1)
 
-    data = pd.concat([data.iloc[:,:3], concat], axis=1)
-
-    #save in bucket
-    BUCKET_NAME = "brain-mnist"
-    data.to_csv(f'gs://{BUCKET_NAME}/other_datasets/{dataset_name}_clean.csv', index=False)
+    data1 = pd.DataFrame(data.eeg.values.tolist())
+    data = pd.concat([data.drop('eeg',axis=1), data1], axis=1)
 
     return data
+
+
+# def map_other_data(data: pd.DataFrame) -> pd.DataFrame:
+#     """
+#     map other data in relevant format:
+#     keep event_index, true_digit, channel & EEG signal
+#     """
+
+#     # min_data_points = data.iloc[:,5].min()
+#     max_data_points = data.iloc[:,5].max()
+
+#     data = data.drop(columns=[0,2,5]) #drop useless columns
+
+#     data.columns = ['index_event', 'channel', 'true_digit', 'eeg'] #rename columns
+#     data = data.reindex(columns=['index_event', 'true_digit', 'channel', 'eeg']) #reorder columns
+
+#     concat = pd.DataFrame()
+#     for i in range(data.shape[0]):
+#       if i%100==0: print('Row ' + str(i) + '/' + str(data.shape[0]))
+#       concat = pd.concat([concat, pd.DataFrame(data.loc[i,'eeg'].split(',')).T], axis=0)
+#     concat.index = list(range(0,data.shape[0]))
+
+#     data = pd.concat([data.iloc[:,:3], concat], axis=1)
+
+#     #save in bucket
+#     BUCKET_NAME = "brain-mnist"
+#     data.to_csv(f'gs://{BUCKET_NAME}/other_datasets/{dataset_name}_clean.csv', index=False)
+
+#     return data
 
 
 
